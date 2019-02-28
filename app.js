41c1514b0f3f8b399e6bebd723d6a9971fbf4876
laszlovad <laszlo.a.vad@gmail.com>
2019-02-28 21:48:24 +0100

    squashed some bugs

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-console.log('The calculator is loaded and ready.');
-// function calculateFuelToOffload() {
-//     var totalDistanceToTravel = prompt('What is the total distance you wish to travel, in kilometers? Enter a number.');
-//     var averageLitersOverHundred = prompt('What is the average fuel consumption of your ship, in liters? Enter a number.');
-//     var tankCapacity = prompt('What is the total capacity of your fuel tank? Enter a number.');
-//     var requiredFuel = totalDistanceToTravel * (averageLitersOverHundred / 100);
-//     var fuelToOffload = tankCapacity - requiredFuel;
-//     alert('Necessary amount of fuel to make the trip: ' + requiredFuel + ' L\n' + 'Amount of fuel you need to offload: ' + fuelToOffload + ' L');
-// }
+function calculateFuelToOffload() {
+    var totalDistanceToTravel = prompt('What is the total distance you wish to travel, in kilometers? Enter a number.');
+    var averageLitersOverHundred = prompt('What is the average fuel consumption of your ship, in liters? Enter a number.');
+    var tankCapacity = prompt('What is the total capacity of your fuel tank? Enter a number.');
+    var requiredFuel = totalDistanceToTravel * (averageLitersOverHundred / 100);
+    var fuelToOffload = tankCapacity - requiredFuel;
+    alert('Necessary amount of fuel to make the trip: ' + requiredFuel + ' L\n' + 'Amount of fuel you need to offload: ' + fuelToOffload + ' L');
+}
 
-// calculateFuelToOffload();
+calculateFuelToOffload();
